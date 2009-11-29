3a19af37b69185ee51f8cd558288ce70634b5d10
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-11-29 22:48:54 +0000

    BraceMatchingTagger is public Ignore build directories
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@26 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Text/Tagging/BraceMatchingTagger.cs
+++ Tvl.VisualStudio.Text/Tagging/BraceMatchingTagger.cs
@@ -11,7 +11,7 @@
     using Microsoft.VisualStudio.Text.Editor;
     using Microsoft.VisualStudio.Text.Tagging;
 
-    internal sealed class BraceMatchingTagger : ITagger<TextMarkerTag>
+    public sealed class BraceMatchingTagger : ITagger<TextMarkerTag>
     {
         public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
 
