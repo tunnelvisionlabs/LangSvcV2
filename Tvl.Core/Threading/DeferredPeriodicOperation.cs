12fdf9bc2d383722c771e74a9fbf53e4b53518fd
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-11-29 23:53:30 +0000

    Copy DeferredPeriodicOperation to Tvl.Core
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@38 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.Core/Threading/DeferredPeriodicOperation.cs
@@ -0,0 +1,104 @@
+﻿namespace Tvl.Threading
+{
+    using System;
+
+    internal class DeferredPeriodicOperation : IDisposable
+    {
+        private Action _action;
+        private System.Timers.Timer _timer;
+        private DateTimeOffset _lastDefer;
+        private TimeSpan _deferPeriod;
+        private TimeSpan _minimumPeriod;
+        private bool _dirty;
+        private int _operating;
+
+        public DeferredPeriodicOperation(Action action, TimeSpan deferPeriod, TimeSpan minimumPeriod, bool requiresInitialOperation)
+        {
+            this._action = action;
+            this._lastDefer = DateTimeOffset.MinValue;
+            this._deferPeriod = deferPeriod;
+            this._minimumPeriod = minimumPeriod;
+            this._dirty = requiresInitialOperation;
+        }
+
+        public TimeSpan DeferPeriod
+        {
+            get
+            {
+                return _deferPeriod;
+            }
+            set
+            {
+                _deferPeriod = value;
+            }
+        }
+
+        public TimeSpan MinimumPeriod
+        {
+            get
+            {
+                return _minimumPeriod;
+            }
+            set
+            {
+                _minimumPeriod = value;
+            }
+        }
+
+        public bool IsDisposed
+        {
+            get;
+            private set;
+        }
+
+        public bool IsDisposing
+        {
+            get;
+            private set;
+        }
+
+        public void Defer()
+        {
+            _lastDefer = DateTimeOffset.Now;
+        }
+
+        public void MarkDirty()
+        {
+            Defer();
+            this._dirty = true;
+        }
+
+        public void Dispose()
+        {
+            bool wasdisposing = IsDisposing;
+            IsDisposing = true;
+            try
+            {
+                Dispose(true);
+            }
+            finally
+            {
+                IsDisposing = wasdisposing;
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDisposed)
+            {
+                _timer.Dispose();
+                _action = null;
+                _timer = null;
+                IsDisposed = true;
+            }
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(GetType().Name);
+        }
+    }
+}
