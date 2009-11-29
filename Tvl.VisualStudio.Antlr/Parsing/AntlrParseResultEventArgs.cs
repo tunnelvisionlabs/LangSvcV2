8cf8f3651fc55602bbef4c0fdbe10c9129038942
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-11-29 23:46:03 +0000

    Refactoring
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@35 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Antlr/Parsing/AntlrParseResultEventArgs.cs
+++ Tvl.VisualStudio.Antlr/Parsing/AntlrParseResultEventArgs.cs
@@ -1,11 +1,10 @@
-﻿namespace JavaLanguageService.AntlrLanguage
+﻿namespace Tvl.VisualStudio.Language.Parsing
 {
     using System.Collections.Generic;
     using Antlr.Runtime;
     using Microsoft.VisualStudio.Text;
-    using Tvl.VisualStudio.Language.Parsing;
 
-    internal class AntlrParseResultEventArgs : ParseResultEventArgs
+    public class AntlrParseResultEventArgs : ParseResultEventArgs
     {
         public AntlrParseResultEventArgs(ITextSnapshot snapshot, IList<ParseErrorEventArgs> errors, ParserRuleReturnScope result)
             : base(snapshot, errors)
