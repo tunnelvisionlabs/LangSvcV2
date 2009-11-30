2a7cf4376b6290cc45652637a8190fe1e6835f06
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-11-30 00:20:15 +0000

    Move AST Explorer to new project Tvl.VisualStudio.Tools.Antlr
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@49 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Tools.Antlr/AstExplorer/AstExplorerProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace JavaLanguageService.AntlrLanguage
+{
+    using System.ComponentModel.Composition;
+    using Tvl.VisualStudio.Shell;
+
+    [Export(typeof(IToolWindowProvider))]
+    public sealed class AstExplorerProvider : IToolWindowProvider
+    {
+        public IToolWindow CreateWindow()
+        {
+            return new ToolWindow<AstExplorerControl>("AST Explorer", null);
+        }
+    }
+}
