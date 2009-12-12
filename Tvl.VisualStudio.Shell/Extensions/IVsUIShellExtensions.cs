b4eacbfe77d8545419bb6ac23791968238a98ecc
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-12-12 22:20:04 +0000

    Add PredefinedWindowMenus Add IVsUIShellExtensions - extension to get tool window panes
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@83 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Shell/Extensions/IVsUIShellExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Tvl.VisualStudio.Shell.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    public static class IVsUIShellExtensions
+    {
+        public static IEnumerable<IVsWindowFrame> GetToolWindows(this IVsUIShell shell)
+        {
+            Contract.Requires<NullReferenceException>(shell != null);
+
+            IEnumWindowFrames frames;
+            ErrorHandler.ThrowOnFailure(shell.GetToolWindowEnum(out frames));
+
+            IVsWindowFrame[] array = new IVsWindowFrame[1];
+            while (true)
+            {
+                uint count;
+                int hr = frames.Next((uint)array.Length, array, out count);
+                ErrorHandler.ThrowOnFailure(hr);
+                if (hr == VSConstants.S_FALSE || count == 0)
+                    break;
+
+                for (uint i = 0; i < count; i++)
+                    yield return array[count];
+            }
+        }
+    }
+}
---
+++ Tvl.VisualStudio.Shell/PredefinedWindowMenus.cs
@@ -0,0 +1,11 @@
+﻿namespace Tvl.VisualStudio.Shell
+{
+    public static class PredefinedWindowMenus
+    {
+        public const string ViewOtherWindows = "view.otherwindows";
+        public const string DebugWindows = "debug.windows";
+        //public const string ArchitectureWindows = "architecture.windows";
+        //public const string TestWindows = "test.windows";
+        //public const string AnalyzeWindows = "analyze.windows";
+    }
+}
