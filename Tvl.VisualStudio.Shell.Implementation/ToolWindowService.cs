e9352e25bfc2eaad38e9f9f5764fdd988e0128e3
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-12-20 20:09:37 +0000

    Tool window commands finally working
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@95 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Shell.Implementation/ToolWindowService.cs
+++ Tvl.VisualStudio.Shell.Implementation/ToolWindowService.cs
@@ -2,27 +2,27 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.Composition;
     using System.Linq;
     using Microsoft.VisualStudio;
-    using Microsoft.VisualStudio.Shell.Interop;
-    using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
-    using Microsoft.VisualStudio.Text.Editor;
-    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.CommandBars;
     using Microsoft.VisualStudio.Editor;
-    using Microsoft.VisualStudio.Utilities;
+    using Microsoft.VisualStudio.Shell.Interop;
+    using Microsoft.VisualStudio.Text.Editor;
     using Microsoft.VisualStudio.TextManager.Interop;
+    using Microsoft.VisualStudio.Utilities;
     using Tvl.VisualStudio.Shell.Extensions;
-    using vsCommandControlType = EnvDTE80.vsCommandControlType;
-    using Microsoft.VisualStudio.Shell;
-    using System.ComponentModel.Design;
     using IOleCommandTarget = Microsoft.VisualStudio.OLE.Interop.IOleCommandTarget;
+    using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
 
     [Export(typeof(IVsTextViewCreationListener))]
-    [ContentType("text")]
+    [ContentType("java")]
     [TextViewRole(PredefinedTextViewRoles.Interactive)]
     public sealed class ToolWindowService : IVsTextViewCreationListener
     {
-        public bool created;
+        private bool created;
+        private readonly Dictionary<uint, Guid> _registeredToolWindows = new Dictionary<uint, Guid>();
+        private CommandTarget _commandTarget;
 
         [Import]
         private IServiceProvider GlobalServiceProvider
@@ -40,36 +40,8 @@
 
         private void RegisterCommandTarget()
         {
-
-        }
-
-        private class CommandTarget : CommandFilter
-        {
-            private ToolWindowService toolWindowService;
-            private uint _cookie;
-
-            protected override IOleCommandTarget Connect()
-            {
-                IVsRegisterPriorityCommandTarget registerPct = (IVsRegisterPriorityCommandTarget)toolWindowService.GlobalServiceProvider.GetService<SVsRegisterPriorityCommandTarget>();
-                registerPct.RegisterPriorityCommandTarget(0, this, out _cookie);
-                return null;
-            }
-
-            protected override void Disconnect()
-            {
-                try
-                {
-                    if (_cookie != 0)
-                    {
-                        IVsRegisterPriorityCommandTarget registerPct = (IVsRegisterPriorityCommandTarget)toolWindowService.GlobalServiceProvider.GetService<SVsRegisterPriorityCommandTarget>();
-                        registerPct.UnregisterPriorityCommandTarget(_cookie);
-                    }
-                }
-                finally
-                {
-                    _cookie = 0;
-                }
-            }
+            _commandTarget = new CommandTarget(this);
+            _commandTarget.Enabled = true;
         }
 
         private void CreateWindows()
@@ -114,28 +86,19 @@
                     if (profferCommands != null)
                     {
                         // Microsoft.VisualStudio.ComponentModelHost.HostPackage
-                        //Guid package = new Guid("{49d12072-378b-4ffa-a09e-40e0b5d097cc}");
-                        // Microsoft.VisualStudio.ExtensionManager.VSPackage.ExtensionManagerPackage
-                        Guid package = new Guid("{E7576C05-1874-450c-9E98-CF3A0897A069}");
-                        //package = new Guid("{AD1A73B0-C489-4c9c-B1FE-EEA54CD19A4F}");
+                        Guid package = new Guid("{49d12072-378b-4ffa-a09e-40e0b5d097cc}");
                         Guid group = typeof(ToolWindowService).GUID;
-                        //group = new Guid("{ADC1BC7B-958B-4548-9F9F-10FC49099825}");
-                        //group = VSConstants.VsStd2010;
-                        //Guid group = new Guid("{23162FF2-3C3F-11d2-890A-0060083196C6}");
                         string pszCmdNameCanonical = "Tvl.ToolWindowService.Show" + lazyProvider.Metadata.Name;
-                        uint pdwCmdId = 1;
+                        uint pdwCmdId;
                         string pszCmdNameLocalized = pszCmdNameCanonical;
-                        string pszBtnText = string.Format("Show the {0} window", caption);
+                        string pszBtnText = string.Format("{0}", caption);
                         string pszCmdTooltip = string.Format("Show the {0} window", caption);
-                        pszCmdTooltip = null;
                         string pszSatelliteDLL = null;
                         uint dwBitmapResourceId = 0;
                         uint dwBitmapImageIndex = 0;
-                        CommandOptions dwCmdFlagsDefault = CommandOptions.DynamicVisibility;
+                        CommandOptions dwCmdFlagsDefault = CommandOptions.None;
                         Guid[] rgguidUIContexts = { };
                         CommandType dwUIElementType = CommandType.Button;
-
-                        profferCommands.RemoveNamedCommand(pszCmdNameCanonical);
 
                         int hr = profferCommands.AddNamedCommand2(
                             ref package,
@@ -153,65 +116,30 @@
                             rgguidUIContexts,
                             (uint)dwUIElementType);
 
+                        if (ErrorHandler.Succeeded(hr))
+                            _registeredToolWindows.Add(pdwCmdId, providerGuid);
+
                         var dteCommands = dte.Commands;
                         var dteCommand = dteCommands.Item(pszCmdNameCanonical);
 
-                        Guid commandGuid;
-                        if (Guid.TryParse(dteCommand.Guid, out commandGuid) && commandGuid != group)
-                        {
-                            group = commandGuid;
-                        }
+                        //Guid mainMenu = VsMenus.guidSHLMainMenu;
+                        //uint IDM_VS_MENU_VIEW = 0x0082;
+                        //object commandBar;
+                        //hr = profferCommands.FindCommandBar(null, ref mainMenu, IDM_VS_MENU_VIEW, out commandBar);
 
-                        if (dteCommand.ID != (int)pdwCmdId)
-                        {
-                            pdwCmdId = (uint)dteCommand.ID;
-                        }
-
-                        IVsShell vsshell = serviceProvider.TryGetGlobalService<SVsShell, IVsShell>();
-                        IVsPackage hostpackage;
-                        if (ErrorHandler.Succeeded(vsshell.IsPackageLoaded(ref package, out hostpackage)))
-                        {
-                            Package hostPackage = hostpackage as Package;
-                            if (hostpackage != null)
-                            {
-                                var mcs = hostPackage.GetService<IMenuCommandService>();
-                                if (mcs != null)
-                                {
-                                    Guid menuGroup = group;
-                                    CommandID showWindowId = new CommandID(menuGroup, (int)pdwCmdId);
-                                    EventHandler invokeHandler = (sender, e) =>
-                                        {
-                                            windowFrame.Show();
-                                        };
-                                    EventHandler changeHandler =
-                                        (sender, e) =>
-                                        {
-                                            return;
-                                        };
-                                    EventHandler beforeQueryStatus =
-                                        (sender, e) =>
-                                        {
-                                            return;
-                                        };
-                                    OleMenuCommand command = new OleMenuCommand(invokeHandler, changeHandler, beforeQueryStatus, showWindowId);
-                                    mcs.AddCommand(command);
-                                }
-                            }
-                        }
-
-                        Guid mainMenu = VsMenus.guidSHLMainMenu;
-                        uint IDM_VS_MENU_VIEW = 0x0082;
-                        object commandBar;
-                        hr = profferCommands.FindCommandBar(null, ref mainMenu, IDM_VS_MENU_VIEW, out commandBar);
-                        dynamic viewMenu = bar1.Controls["View"];
-                        dynamic control = dteCommand.AddControl(viewMenu.Controls.Parent);
+                        CommandBarPopup viewMenu = bar1.Controls["View"];
+                        CommandBarPopup otherWindows = (CommandBarPopup)viewMenu.Controls["Other Windows"];
+                        CommandBarButton control = (CommandBarButton)dteCommand.AddControl(otherWindows.Controls.Parent);
                         control.Visible = true;
                         control.Enabled = true;
                         control.Caption = pszBtnText;
                     }
-
-                    //windowFrame.ShowNoActivate();
                 }
+            }
+
+            if (ToolWindowProviders.Any())
+            {
+                RegisterCommandTarget();
             }
         }
 
@@ -225,8 +153,78 @@
                     CreateWindows();
                 }
             }
-            catch
+            catch (Exception ex)
             {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
+            }
+        }
+
+        private class CommandTarget : CommandFilter
+        {
+            private ToolWindowService toolWindowService;
+            private uint _cookie;
+
+            public CommandTarget(ToolWindowService toolWindowService)
+            {
+                this.toolWindowService = toolWindowService;
+            }
+
+            protected override bool HandlePreExec(ref Guid commandGroup, uint commandId, uint executionOptions, IntPtr pvaIn, IntPtr pvaOut)
+            {
+                if (commandGroup == typeof(ToolWindowService).GUID)
+                {
+                    IVsUIShell shell = (IVsUIShell)toolWindowService.GlobalServiceProvider.GetService<SVsUIShell>();
+
+                    __VSCREATETOOLWIN flags = __VSCREATETOOLWIN.CTW_fForceCreate;
+                    Guid slot;
+                    if (!toolWindowService._registeredToolWindows.TryGetValue(commandId, out slot))
+                        return false;
+
+                    IVsWindowFrame frame;
+                    if (ErrorHandler.Failed(shell.FindToolWindow((uint)flags, ref slot, out frame)))
+                        return false;
+
+                    frame.Show();
+                    return true;
+                }
+
+                return false;
+            }
+
+            protected override CommandStatus QueryCommandStatus(ref Guid group, uint id)
+            {
+                if (group == typeof(ToolWindowService).GUID)
+                {
+                    if (toolWindowService._registeredToolWindows.ContainsKey(id))
+                        return CommandStatus.Supported | CommandStatus.Enabled;
+                    return CommandStatus.Supported;
+                }
+
+                return base.QueryCommandStatus(ref group, id);
+            }
+
+            protected override IOleCommandTarget Connect()
+            {
+                IVsRegisterPriorityCommandTarget registerPct = (IVsRegisterPriorityCommandTarget)toolWindowService.GlobalServiceProvider.GetService<SVsRegisterPriorityCommandTarget>();
+                registerPct.RegisterPriorityCommandTarget(0, this, out _cookie);
+                return null;
+            }
+
+            protected override void Disconnect()
+            {
+                try
+                {
+                    if (_cookie != 0)
+                    {
+                        IVsRegisterPriorityCommandTarget registerPct = (IVsRegisterPriorityCommandTarget)toolWindowService.GlobalServiceProvider.GetService<SVsRegisterPriorityCommandTarget>();
+                        registerPct.UnregisterPriorityCommandTarget(_cookie);
+                    }
+                }
+                finally
+                {
+                    _cookie = 0;
+                }
             }
         }
     }
