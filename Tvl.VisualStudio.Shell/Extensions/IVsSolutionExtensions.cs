eedc90aece2b41d852f177756094b8c6c2c1e782
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-12-28 21:53:18 +0000

    Add extension methods for IVsHierarchy Add extension methods for IVsSolution
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@104 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Shell/Extensions/IVsSolutionExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace Tvl.VisualStudio.Shell.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    public static class IVsSolutionExtensions
+    {
+        public static IEnumerable<IVsHierarchy> GetProjectHierarchies(IVsSolution solution)
+        {
+            return GetProjectHierarchies(solution, __VSENUMPROJFLAGS.EPF_LOADEDINSOLUTION);
+        }
+
+        public static IEnumerable<IVsHierarchy> GetProjectHierarchies(IVsSolution solution, __VSENUMPROJFLAGS flags)
+        {
+            if (solution == null)
+                throw new ArgumentNullException("solution");
+
+            Guid empty = Guid.Empty;
+            IEnumHierarchies ppenum;
+            if (ErrorHandler.Succeeded(solution.GetProjectEnum((uint)flags, ref empty, out ppenum)))
+            {
+                if (ppenum != null)
+                {
+                    IVsHierarchy[] rgelt = new IVsHierarchy[1];
+                    uint celtFetched;
+                    for (; ; )
+                    {
+                        int hr = ppenum.Next((uint)rgelt.Length, rgelt, out celtFetched);
+                        ErrorHandler.ThrowOnFailure(hr);
+
+                        for (int i = 0; i < celtFetched; i++)
+                            yield return rgelt[i];
+
+                        if (hr == VSConstants.S_FALSE)
+                            yield break;
+                    }
+                }
+            }
+        }
+    }
+}
