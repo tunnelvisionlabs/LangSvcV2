c0d08b9b214213f303dd87a5563f98709be92da2
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2009-12-28 21:54:51 +0000

    Solution file list MEF tool window updates
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@105 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Tools.Antlr/FileList/SolutionFileListControl.xaml.cs
+++ Tvl.VisualStudio.Tools.Antlr/FileList/SolutionFileListControl.xaml.cs
@@ -1,34 +1,38 @@
 ﻿namespace Tvl.VisualStudio.Tools.FileList
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Runtime.InteropServices;
     using System.Linq;
-    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading;
     using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
     using System.Windows.Documents;
-    using System.Windows.Input;
     using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
-    using System.Collections.Concurrent;
-    using Microsoft.VisualStudio.Shell.Interop;
     using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell.Interop;
     using Tvl.VisualStudio.Shell.Extensions;
-    using System.Runtime.InteropServices;
+    using Path = System.IO.Path;
+    using Microsoft.VisualStudio.Shell;
+    using File = System.IO.File;
+    using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
 
     public partial class SolutionFileListControl : UserControl
     {
-        private static readonly ConcurrentDictionary<string, ImageSource> _fileIcons =
-            new ConcurrentDictionary<string, ImageSource>(StringComparer.OrdinalIgnoreCase);
-
-        private SolutionFileListProvider _provider;
+        private readonly SolutionFileListProvider _provider;
+        private readonly List<ProjectItemInfo> _items;
 
         internal SolutionFileListControl(SolutionFileListProvider provider)
         {
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+            Contract.EndContractBlock();
+
             this._provider = provider;
+            this._items = new List<ProjectItemInfo>();
             InitializeComponent();
         }
 
@@ -60,88 +64,12 @@
             }
         }
 
-#if false
-        private delegate void ProcessHierarchyNode(IVsHierarchy hierarchyNode, uint itemid, int recursionLevel);
-
-        private static void EnumHierarchyItems(IVsHierarchy hierarchy, uint itemid, int recursionLevel, bool isSolution, bool visibleOnly, ProcessHierarchyNode processNode)
-        {
-            EnumHierarchyItems(hierarchy, itemid, recursionLevel, isSolution, visibleOnly, false, processNode);
-        }
-
-        private static void EnumHierarchyItems(IVsHierarchy hierarchy, uint itemid, int recursionLevel, bool isSolution, bool visibleOnly, bool nothrow, ProcessHierarchyNode processNode)
-        {
-            int hr;
-            IntPtr nestedHierarchyObj;
-            uint nestedItemId;
-            Guid hierGuid = typeof(IVsHierarchy).GUID;
-
-            hr = hierarchy.GetNestedHierarchy(itemid, ref hierGuid, out nestedHierarchyObj, out nestedItemId);
-            if (hr == VSConstants.S_OK && nestedHierarchyObj != IntPtr.Zero)
-            {
-                IVsHierarchy nestedHierarchy = Marshal.GetObjectForIUnknown(nestedHierarchyObj) as IVsHierarchy;
-                Marshal.Release(nestedHierarchyObj);
-                if (nestedHierarchy != null)
-                {
-                    EnumHierarchyItems(nestedHierarchy, nestedItemId, recursionLevel, false, visibleOnly, nothrow, processNode);
-                }
-            }
-            else
-            {
-                object pVar;
-
-                processNode(hierarchy, itemid, recursionLevel);
-
-                recursionLevel++;
-
-                hr = hierarchy.GetProperty(itemid, ((visibleOnly || (isSolution && recursionLevel == 1)) ? (int)__VSHPROPID.VSHPROPID_FirstVisibleChild : (int)__VSHPROPID.VSHPROPID_FirstChild), out pVar);
-                if (!nothrow)
-                    ErrorHandler.ThrowOnFailure(hr);
-
-                if (hr == VSConstants.S_OK)
-                {
-                    uint childId = GetItemId(pVar);
-                    while (childId != VSConstants.VSITEMID_NIL)
-                    {
-                        EnumHierarchyItems(hierarchy, childId, recursionLevel, false, visibleOnly, nothrow, processNode);
-
-                        hr = hierarchy.GetProperty(childId, ((visibleOnly || (isSolution && recursionLevel == 1)) ? (int)__VSHPROPID.VSHPROPID_NextVisibleSibling : (int)__VSHPROPID.VSHPROPID_NextSibling), out pVar);
-                        if (!nothrow)
-                            ErrorHandler.ThrowOnFailure(hr);
-
-                        if (hr == VSConstants.S_OK)
-                        {
-                            childId = GetItemId(pVar);
-                        }
-                        else
-                        {
-                            childId = VSConstants.VSITEMID_NIL;
-                        }
-                    }
-                }
-            }
-        }
-
-        private static uint GetItemId(object pvar)
-        {
-            if (pvar == null)
-                return VSConstants.VSITEMID_NIL;
-            if (pvar is int)
-                return (uint)(int)pvar;
-            if (pvar is uint)
-                return (uint)pvar;
-            if (pvar is short)
-                return (uint)(short)pvar;
-            if (pvar is ushort)
-                return (uint)(ushort)pvar;
-            if (pvar is long)
-                return (uint)(long)pvar;
-            return VSConstants.VSITEMID_NIL;
-        }
-
         private void UpdateFiles()
         {
             try
             {
+                this._items.Clear();
+
                 IVsSolution solution = (IVsSolution)_provider.ServiceProvider.GetService<SVsSolution>();
                 if (solution == null)
                     return;
@@ -149,6 +77,12 @@
                 List<ProjectItemInfo> items = new List<ProjectItemInfo>();
                 foreach (IVsHierarchy projectHierarchy in GetProjectHierarchies(solution))
                 {
+                    EnvDTE.Project project = projectHierarchy.GetExtensibilityObjectOrDefault(VSConstants.VSITEMID_ROOT) as EnvDTE.Project;
+                    if (project == null)
+                        continue;
+
+                    string projectName = project.Name;
+
                     projectHierarchy.EnumHierarchyItems(VSConstants.VSITEMID_ROOT, 0, false, true, true,
                         (node, itemId, level) =>
                         {
@@ -165,11 +99,11 @@
 
                                         ProjectItemInfo itemInfo = new ProjectItemInfo()
                                         {
-                                            filename = System.IO.Path.GetFileName(filename),
-                                            project = projectName,
-                                            path = filename
+                                            FileName = System.IO.Path.GetFileName(filename),
+                                            Project = projectName,
+                                            Path = filename
                                         };
-                                        _items.Add(itemInfo);
+                                        this._items.Add(itemInfo);
                                     }
                                 }
                             }
@@ -181,46 +115,232 @@
                             }
                         });
                 }
-
-                //var extensions = proj
             }
-            catch
+            catch (Exception ex)
             {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
             }
         }
 
         private void UpdateList()
         {
-        }
-#endif
-
-        public class ProjectItemInfo
-        {
-            public string FileName
+            if (Thread.CurrentThread != lstResults.Dispatcher.Thread)
             {
-                get;
-                set;
+                lstResults.Dispatcher.Invoke((Action)UpdateList);
+                return;
             }
 
-            public string Project
+            try
             {
-                get;
-                set;
-            }
+                bool caseSensitive = this.CaseSensitive;
+                string filter = this.Filter;
 
-            public string Path
-            {
-                get;
-                set;
-            }
-
-            public ImageSource Icon
-            {
-                get
+                Regex rx = null;
+                try
                 {
-                    Func<string, ImageSource> valueFactory = null;
-                    return _fileIcons.GetOrAdd(System.IO.Path.GetExtension(FileName), valueFactory);
+                    if (UseRegex)
+                        rx = new Regex(filter, caseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
                 }
+                catch (ArgumentException)
+                {
+                    lstResults.Items.Clear();
+                    txtQuery.Background = Brushes.LightSalmon;
+                    return;
+                }
+
+                var filtered = this._items.FindAll(
+                    item =>
+                    {
+                        if (item == null || item.FileName == null)
+                            return false;
+
+                        if (rx == null)
+                        {
+                            if (!caseSensitive)
+                                return item.FileName.ToLower().Contains(filter.ToLower());
+                            else
+                                return item.FileName.Contains(filter);
+                        }
+
+                        return rx.IsMatch(item.FileName);
+                    });
+
+                lstResults.Items.Clear();
+                foreach (var item in filtered)
+                    lstResults.Items.Add(item);
+
+                txtQuery.Background = Brushes.White;
+
+                // TODO: default selection
+            }
+            catch (Exception ex)
+            {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
+
+                try
+                {
+                    Action setColor = () => txtQuery.Background = Brushes.LightSalmon;
+                    txtQuery.Dispatcher.Invoke(setColor);
+                }
+                catch (Exception ex2)
+                {
+                    if (ErrorHandler.IsCriticalException(ex2))
+                        throw;
+                }
+            }
+        }
+
+        private void OnRefreshClick(object sender, RoutedEventArgs e)
+        {
+            UpdateFiles();
+            UpdateList();
+        }
+
+        private void OnFilterChanged(object sender, RoutedEventArgs e)
+        {
+            if (Thread.CurrentThread != txtQuery.Dispatcher.Thread)
+            {
+                Action action = () => OnFilterChanged(sender, e);
+                txtQuery.Dispatcher.Invoke(action);
+                return;
+            }
+
+            if (txtQuery.Text.StartsWith("/") && !chkRegex.IsChecked.HasValue || !chkRegex.IsChecked.Value)
+            {
+                chkRegex.IsChecked = true;
+                chkRegex.IsEnabled = false;
+            }
+            else if (!chkRegex.IsEnabled)
+            {
+                chkRegex.IsChecked = false;
+                chkRegex.IsEnabled = true;
+            }
+
+            UpdateList();
+        }
+
+        private void OnOpenSelectedFiles(object sender, RoutedEventArgs e)
+        {
+            OpenSelectedFiles(lstResults.SelectedItems.OfType<ProjectItemInfo>());
+        }
+
+        private void OpenSelectedFiles(IEnumerable<ProjectItemInfo> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException("items");
+            Contract.EndContractBlock();
+
+            IServiceProvider serviceProvider = _provider.ServiceProvider;
+            IVsUIShellOpenDocument service = serviceProvider.GetService<IVsUIShellOpenDocument>();
+            IVsRunningDocumentTable table = serviceProvider.GetService<IVsRunningDocumentTable>();
+
+            foreach (var item in items)
+            {
+                string path = item.Path;
+                if (string.IsNullOrEmpty(path) || !File.Exists(path))
+                    continue;
+
+                IVsUIHierarchy uiHierarchy = null;
+                uint itemId = uint.MaxValue;
+                IVsWindowFrame frame = null;
+                Guid logicalView = Guid.Empty;
+                bool isOpen = false;
+
+                if (service != null && table != null)
+                {
+                    IntPtr handle = IntPtr.Zero;
+                    try
+                    {
+                        IVsHierarchy hierarchy;
+                        uint cookie;
+                        int hr = table.FindAndLockDocument(0, path, out hierarchy, out itemId, out handle, out cookie);
+                        ErrorHandler.ThrowOnFailure(hr);
+                        uint[] pitemidOpen = new uint[1];
+                        uint ido = (logicalView == Guid.Empty) ? 2U : 0U;
+                        int open;
+                        hr = service.IsDocumentOpen((IVsUIHierarchy)hierarchy, itemId, path, ref logicalView, ido, out uiHierarchy, pitemidOpen, out frame, out open);
+                        ErrorHandler.ThrowOnFailure(hr);
+                        if (frame != null)
+                        {
+                            itemId = pitemidOpen[0];
+                            isOpen = open != 0;
+                        }
+                    }
+                    finally
+                    {
+                        if (handle != IntPtr.Zero)
+                            Marshal.Release(handle);
+                    }
+                }
+
+                if (!isOpen)
+                {
+                    if (service != null)
+                    {
+                        IOleServiceProvider oleServiceProvider;
+                        int hr = service.OpenDocumentViaProject(path, ref logicalView, out oleServiceProvider, out uiHierarchy, out itemId, out frame);
+                        ErrorHandler.ThrowOnFailure(hr);
+                    }
+                }
+
+                if (frame != null)
+                {
+                    int hr = frame.Show();
+                    ErrorHandler.ThrowOnFailure(hr);
+                }
+            }
+        }
+    }
+
+    public class ProjectItemInfo
+    {
+        private static readonly ConcurrentDictionary<string, ImageSource> _fileIcons =
+            new ConcurrentDictionary<string, ImageSource>(StringComparer.OrdinalIgnoreCase);
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public string Project
+        {
+            get;
+            set;
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        public ImageSource Icon
+        {
+            get
+            {
+                return _fileIcons.GetOrAdd(System.IO.Path.GetExtension(FileName), IconFactory);
+            }
+        }
+
+        private ImageSource IconFactory(string extension)
+        {
+            return LoadIcon();
+        }
+
+        private ImageSource LoadIcon()
+        {
+            using (System.Drawing.Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(Path))
+            {
+                // This new call in WPF finally allows us to read/display 32bit Windows file icons!
+                ImageSource source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                  sysicon.Handle,
+                  System.Windows.Int32Rect.Empty,
+                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                return source;
             }
         }
     }
