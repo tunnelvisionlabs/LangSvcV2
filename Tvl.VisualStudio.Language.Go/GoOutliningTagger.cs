17ca4562899fcf27248923be2f5c95877aeaad10
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2010-01-16 20:11:46 +0000

    Initial support for code outlining in Go
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@127 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Language.Go/GoOutliningTagger.cs
@@ -0,0 +1,101 @@
+﻿namespace Tvl.VisualStudio.Language.Go
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using Antlr.Runtime.Tree;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Tvl.VisualStudio.Language.Parsing;
+
+    internal sealed class GoOutliningTagger : ITagger<IOutliningRegionTag>
+    {
+        private List<ITagSpan<IOutliningRegionTag>> _outliningRegions;
+        private readonly GoOutliningTaggerProvider _provider;
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        public GoOutliningTagger(ITextBuffer textBuffer, IBackgroundParser backgroundParser, GoOutliningTaggerProvider provider)
+        {
+            if (textBuffer == null)
+                throw new ArgumentNullException("textBuffer");
+            if (backgroundParser == null)
+                throw new ArgumentNullException("backgroundParser");
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+            Contract.EndContractBlock();
+
+            this.TextBuffer = textBuffer;
+            this.BackgroundParser = backgroundParser;
+            this._provider = provider;
+
+            this._outliningRegions = new List<ITagSpan<IOutliningRegionTag>>();
+
+            this.BackgroundParser.ParseComplete += OnBackgroundParseComplete;
+        }
+
+        private ITextBuffer TextBuffer
+        {
+            get;
+            set;
+        }
+
+        private IBackgroundParser BackgroundParser
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<ITagSpan<IOutliningRegionTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            return _outliningRegions;
+        }
+
+        private void OnTagsChanged(SnapshotSpanEventArgs e)
+        {
+            var t = TagsChanged;
+            if (t != null)
+                t(this, e);
+        }
+
+        private void OnBackgroundParseComplete(object sender, ParseResultEventArgs e)
+        {
+            AntlrParseResultEventArgs antlrParseResultArgs = e as AntlrParseResultEventArgs;
+            List<ITagSpan<IOutliningRegionTag>> outliningRegions = new List<ITagSpan<IOutliningRegionTag>>();
+            if (antlrParseResultArgs != null)
+            {
+                var result = antlrParseResultArgs.Result.Tree as CommonTree;
+                if (result != null)
+                {
+                    foreach (CommonTree child in result.Children)
+                    {
+                        if (child == null || string.IsNullOrEmpty(child.Text))
+                            continue;
+
+                        switch (child.Type)
+                        {
+                        case GoLexer.KW_IMPORT:
+                        case GoLexer.KW_TYPE:
+                        case GoLexer.KW_CONST:
+                        case GoLexer.KW_FUNC:
+                            var startToken = antlrParseResultArgs.Tokens[child.TokenStartIndex];
+                            var stopToken = antlrParseResultArgs.Tokens[child.TokenStopIndex];
+                            Span span = new Span(startToken.StartIndex, stopToken.StopIndex - startToken.StartIndex + 1);
+                            SnapshotSpan snapshotSpan = new SnapshotSpan(e.Snapshot, span);
+                            IOutliningRegionTag tag = new OutliningRegionTag();
+                            TagSpan<IOutliningRegionTag> tagSpan = new TagSpan<IOutliningRegionTag>(snapshotSpan, tag);
+                            outliningRegions.Add(tagSpan);
+                            break;
+
+                        default:
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            this._outliningRegions = outliningRegions;
+            OnTagsChanged(new SnapshotSpanEventArgs(new SnapshotSpan(e.Snapshot, new Span(0, e.Snapshot.Length))));
+        }
+    }
+}
---
+++ Tvl.VisualStudio.Language.Go/GoOutliningTaggerProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace Tvl.VisualStudio.Language.Go
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+    using Tvl.VisualStudio.Language.Parsing;
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType(GoConstants.GoContentType)]
+    [TagType(typeof(IOutliningRegionTag))]
+    public sealed class GoOutliningTaggerProvider : ITaggerProvider
+    {
+        [Import]
+        private IBackgroundParserFactoryService BackgroundParserFactoryService
+        {
+            get;
+            set;
+        }
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            Func<GoOutliningTagger> creator = () => new GoOutliningTagger(buffer, BackgroundParserFactoryService.GetBackgroundParser(buffer), this);
+            return buffer.Properties.GetOrCreateSingletonProperty(creator) as ITagger<T>;
+        }
+    }
+}
