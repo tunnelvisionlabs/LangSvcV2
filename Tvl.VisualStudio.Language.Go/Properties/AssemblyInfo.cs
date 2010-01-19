7152e7cb658d4de343dabe5d6924e5796268a0de
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2010-01-19 01:04:49 +0000

    Tvl.VisualStudio.Language.Go is not CLSCompliant
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@128 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Language.Go/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.Language.Go/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -18,6 +19,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
 
 // Version information for an assembly consists of the following four values:
 //
