d0a8277e317c8a491aa29966569f08ed1d264c58
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-01-01 01:31:38 +0000

    Export AlloyQuickInfoSourceProvider
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@184 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Language.Alloy/AlloyQuickInfoSourceProvider.cs
+++ Tvl.VisualStudio.Language.Alloy/AlloyQuickInfoSourceProvider.cs
@@ -1,9 +1,15 @@
 ﻿namespace Tvl.VisualStudio.Language.Alloy
 {
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Utilities;
     using IQuickInfoSource = Microsoft.VisualStudio.Language.Intellisense.IQuickInfoSource;
     using IQuickInfoSourceProvider = Microsoft.VisualStudio.Language.Intellisense.IQuickInfoSourceProvider;
     using ITextBuffer = Microsoft.VisualStudio.Text.ITextBuffer;
 
+    [Export(typeof(IQuickInfoSourceProvider))]
+    [Order]
+    [ContentType(AlloyConstants.AlloyContentType)]
+    [Name("AlloyQuickInfoSource")]
     internal class AlloyQuickInfoSourceProvider : IQuickInfoSourceProvider
     {
         public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
