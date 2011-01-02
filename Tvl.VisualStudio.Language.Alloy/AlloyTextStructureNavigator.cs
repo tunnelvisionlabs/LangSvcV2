70d594ba1abc1787325f5a8bad5e3df256812d6c
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-01-02 18:18:45 +0000

    Add a stub implementation of ITextStructureNavigator for Alloy
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@195 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Language.Alloy/AlloyTextStructureNavigator.cs
@@ -0,0 +1,58 @@
+﻿namespace Tvl.VisualStudio.Language.Alloy
+{
+    using System;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Operations;
+    using Microsoft.VisualStudio.Utilities;
+
+    internal class AlloyTextStructureNavigator : ITextStructureNavigator
+    {
+        //private readonly AlloyTextStructureNavigatorProvider _provider;
+        private readonly ITextBuffer _textBuffer;
+        private readonly ITextStructureNavigator _delegateTextStructureNavigator;
+
+        public AlloyTextStructureNavigator(ITextBuffer textBuffer, ITextStructureNavigator delegateTextStructureNavigator)
+        {
+            if (textBuffer == null)
+                throw new ArgumentNullException("textBuffer");
+            if (delegateTextStructureNavigator == null)
+                throw new ArgumentNullException("delegateTextStructureNavigator");
+
+            this._textBuffer = textBuffer;
+            this._delegateTextStructureNavigator = delegateTextStructureNavigator;
+        }
+
+        public IContentType ContentType
+        {
+            get
+            {
+                return _textBuffer.ContentType;
+            }
+        }
+
+        public TextExtent GetExtentOfWord(SnapshotPoint currentPosition)
+        {
+            return _delegateTextStructureNavigator.GetExtentOfWord(currentPosition);
+        }
+
+        public SnapshotSpan GetSpanOfEnclosing(SnapshotSpan activeSpan)
+        {
+            return _delegateTextStructureNavigator.GetSpanOfEnclosing(activeSpan);
+        }
+
+        public SnapshotSpan GetSpanOfFirstChild(SnapshotSpan activeSpan)
+        {
+            return _delegateTextStructureNavigator.GetSpanOfFirstChild(activeSpan);
+        }
+
+        public SnapshotSpan GetSpanOfNextSibling(SnapshotSpan activeSpan)
+        {
+            return _delegateTextStructureNavigator.GetSpanOfNextSibling(activeSpan);
+        }
+
+        public SnapshotSpan GetSpanOfPreviousSibling(SnapshotSpan activeSpan)
+        {
+            return _delegateTextStructureNavigator.GetSpanOfPreviousSibling(activeSpan);
+        }
+    }
+}
---
+++ Tvl.VisualStudio.Language.Alloy/AlloyTextStructureNavigatorProvider.cs
@@ -0,0 +1,51 @@
+﻿namespace Tvl.VisualStudio.Language.Alloy
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Operations;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(ITextStructureNavigatorProvider))]
+    [ContentType(AlloyConstants.AlloyContentType)]
+    internal class AlloyTextStructureNavigatorProvider : ITextStructureNavigatorProvider, IPartImportsSatisfiedNotification
+    {
+        private IContentType _anyContentType;
+
+        [Import]
+        private ITextStructureNavigatorSelectorService TextStructureNavigatorSelectorService
+        {
+            get;
+            set;
+        }
+
+        [Import]
+        private IContentTypeRegistryService ContentTypeRegistryService
+        {
+            get;
+            set;
+        }
+
+        private IContentType AnyContentType
+        {
+            get
+            {
+                return _anyContentType;
+            }
+        }
+
+        public ITextStructureNavigator CreateTextStructureNavigator(ITextBuffer textBuffer)
+        {
+            if (textBuffer == null)
+                throw new ArgumentNullException("textBuffer");
+
+            ITextStructureNavigator delegateTextStructureNavigator = TextStructureNavigatorSelectorService.CreateTextStructureNavigator(textBuffer, AnyContentType);
+            return new AlloyTextStructureNavigator(textBuffer, delegateTextStructureNavigator);
+        }
+
+        void IPartImportsSatisfiedNotification.OnImportsSatisfied()
+        {
+            _anyContentType = ContentTypeRegistryService.GetContentType("any");
+        }
+    }
+}
