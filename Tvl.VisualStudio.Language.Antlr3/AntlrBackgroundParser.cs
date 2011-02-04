a4ff7409cd431997f3915350f5e1508c58ba10df
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-02-04 03:46:03 +0000

    Maintain a list of current rule spans and seek points in AntlrBackgroundParser
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@213 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Language.Antlr3/AntlrBackgroundParser.cs
+++ Tvl.VisualStudio.Language.Antlr3/AntlrBackgroundParser.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using Antlr.Runtime;
+    using Antlr.Runtime.Tree;
     using global::Antlr3;
     using global::Antlr3.Tool;
     using Microsoft.VisualStudio.Text;
@@ -23,6 +25,12 @@
         }
 
         public AntlrParseResultEventArgs PreviousParseResult
+        {
+            get;
+            private set;
+        }
+
+        public Dictionary<string, KeyValuePair<ITrackingSpan, ITrackingPoint>> RuleSpans
         {
             get;
             private set;
@@ -79,7 +87,78 @@
         protected override void OnParseComplete(ParseResultEventArgs e)
         {
             PreviousParseResult = e as AntlrParseResultEventArgs;
+            ExtractRuleSpans();
             base.OnParseComplete(e);
+        }
+
+        private void ExtractRuleSpans()
+        {
+            Dictionary<string, KeyValuePair<ITrackingSpan, ITrackingPoint>> rules = new Dictionary<string, KeyValuePair<ITrackingSpan, ITrackingPoint>>();
+            var antlrParseResultArgs = PreviousParseResult;
+            if (antlrParseResultArgs != null)
+            {
+                IAstRuleReturnScope resultArgs = antlrParseResultArgs.Result as IAstRuleReturnScope;
+                var result = resultArgs != null ? resultArgs.Tree as CommonTree : null;
+                if (result != null)
+                {
+                    foreach (CommonTree child in result.Children)
+                    {
+                        if (child == null || string.IsNullOrEmpty(child.Text))
+                            continue;
+
+                        if (child.Text == "rule" && child.ChildCount > 0)
+                        {
+                            var ruleName = child.GetChild(0).Text;
+                            if (string.IsNullOrEmpty(ruleName))
+                                continue;
+
+                            if (ruleName == "Tokens")
+                                continue;
+
+                            IToken startToken = antlrParseResultArgs.Tokens[child.TokenStartIndex];
+                            IToken stopToken = antlrParseResultArgs.Tokens[child.TokenStopIndex];
+                            Span span = new Span(startToken.StartIndex, stopToken.StopIndex - startToken.StartIndex + 1);
+                            ITrackingSpan trackingSpan = antlrParseResultArgs.Snapshot.CreateTrackingSpan(span, SpanTrackingMode.EdgeNegative);
+                            ITrackingPoint trackingPoint = antlrParseResultArgs.Snapshot.CreateTrackingPoint(((CommonTree)child.GetChild(0)).Token.StartIndex, PointTrackingMode.Negative);
+                            rules[ruleName] = new KeyValuePair<ITrackingSpan, ITrackingPoint>(trackingSpan, trackingPoint);
+                        }
+                        else if (child.Text.StartsWith("tokens"))
+                        {
+                            foreach (CommonTree tokenChild in child.Children)
+                            {
+                                if (tokenChild.Text == "=" && tokenChild.ChildCount == 2)
+                                {
+                                    var ruleName = tokenChild.GetChild(0).Text;
+                                    if (string.IsNullOrEmpty(ruleName))
+                                        continue;
+
+                                    IToken startToken = antlrParseResultArgs.Tokens[tokenChild.TokenStartIndex];
+                                    IToken stopToken = antlrParseResultArgs.Tokens[tokenChild.TokenStopIndex];
+                                    Span span = new Span(startToken.StartIndex, stopToken.StopIndex - startToken.StartIndex + 1);
+                                    ITrackingSpan trackingSpan = antlrParseResultArgs.Snapshot.CreateTrackingSpan(span, SpanTrackingMode.EdgeNegative);
+                                    ITrackingPoint trackingPoint = antlrParseResultArgs.Snapshot.CreateTrackingPoint(((CommonTree)tokenChild.GetChild(0)).Token.StartIndex, PointTrackingMode.Negative);
+                                    rules[ruleName] = new KeyValuePair<ITrackingSpan, ITrackingPoint>(trackingSpan, trackingPoint);
+                                }
+                                else if (tokenChild.ChildCount == 0)
+                                {
+                                    var ruleName = tokenChild.Text;
+                                    if (string.IsNullOrEmpty(ruleName))
+                                        continue;
+
+                                    IToken startToken = antlrParseResultArgs.Tokens[tokenChild.TokenStartIndex];
+                                    IToken stopToken = antlrParseResultArgs.Tokens[tokenChild.TokenStopIndex];
+                                    Span span = new Span(startToken.StartIndex, stopToken.StopIndex - startToken.StartIndex + 1);
+                                    ITrackingSpan trackingSpan = antlrParseResultArgs.Snapshot.CreateTrackingSpan(span, SpanTrackingMode.EdgeNegative);
+                                    ITrackingPoint trackingPoint = antlrParseResultArgs.Snapshot.CreateTrackingPoint(tokenChild.Token.StartIndex, PointTrackingMode.Negative);
+                                    rules[ruleName] = new KeyValuePair<ITrackingSpan, ITrackingPoint>(trackingSpan, trackingPoint);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            RuleSpans = rules;
         }
     }
 }
