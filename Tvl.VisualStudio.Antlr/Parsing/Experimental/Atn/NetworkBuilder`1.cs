d09ef545b7caa3dc81c05ef9fa3e4bcb82dd234a
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-07-21 22:29:49 +0000

    Synchronize the singleton network creation in NetworkBuilder<T>
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@365 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Antlr/Parsing/Experimental/Atn/NetworkBuilder`1.cs
+++ Tvl.VisualStudio.Antlr/Parsing/Experimental/Atn/NetworkBuilder`1.cs
@@ -4,15 +4,22 @@
         where T : NetworkBuilder, new()
     {
         private static Network _network;
+        private static readonly object _lock = new object();
 
         public static Network GetOrBuildNetwork()
         {
             if (_network != null)
                 return _network;
 
-            NetworkBuilder builder = new T();
-            _network = builder.BuildNetwork();
-            return _network;
+            lock (_lock)
+            {
+                if (_network != null)
+                    return _network;
+
+                NetworkBuilder builder = new T();
+                _network = builder.BuildNetwork();
+                return _network;
+            }
         }
     }
 }
