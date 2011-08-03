8661db9a629b7cb8c3155eb1126e1c3d2bc64a0b
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-08-03 13:55:26 +0000

    Add the AntlrTextStructureNavigator so $identifier gets treated as a single word by GetExtentOfWord
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@428 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Language.Antlr3/AntlrTextStructureNavigatorProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace Tvl.VisualStudio.Language.Antlr3
+{
+    using System.ComponentModel.Composition;
+    using System.Linq;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Operations;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(ITextStructureNavigatorProvider))]
+    [ContentType(AntlrConstants.AntlrContentType)]
+    internal class AntlrTextStructureNavigatorProvider : ITextStructureNavigatorProvider
+    {
+        [Import]
+        public IContentTypeRegistryService ContentTypeRegistryService
+        {
+            get;
+            private set;
+        }
+
+        [Import]
+        public ITextStructureNavigatorSelectorService TextStructureNavigatorSelectorService
+        {
+            get;
+            private set;
+        }
+
+        public ITextStructureNavigator CreateTextStructureNavigator(ITextBuffer textBuffer)
+        {
+            IContentType contentType = ContentTypeRegistryService.GetContentType(AntlrConstants.AntlrContentType);
+            IContentType baseContentType = contentType.BaseTypes.First();
+            ITextStructureNavigator delegateNavigator = TextStructureNavigatorSelectorService.CreateTextStructureNavigator(textBuffer, baseContentType);
+            return new AntlrTextStructureNavigator(textBuffer, delegateNavigator);
+        }
+    }
+}
