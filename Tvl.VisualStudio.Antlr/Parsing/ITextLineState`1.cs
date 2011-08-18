b6c640736461ef907326d970a63ee1f54957b4cc
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-08-18 14:31:34 +0000

    Add AntlrTaggerBase<TState, TTag> (basic implementation of ITagger<TTag>)
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@469 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Antlr/Parsing/ITextLineState`1.cs
@@ -0,0 +1,20 @@
+﻿namespace Tvl.VisualStudio.Language.Parsing
+{
+    public interface ITextLineState<T>
+        where T : struct, ITextLineState<T>
+    {
+        bool IsDirty
+        {
+            get;
+        }
+
+        bool IsMultiline
+        {
+            get;
+        }
+
+        T CreateDirtyState();
+
+        T CreateMultilineState();
+    }
+}
