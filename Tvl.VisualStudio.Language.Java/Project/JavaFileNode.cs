306efce3588cec71fc2b70f08bddb155e99577a1
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-08-18 14:34:47 +0000

    Java files can be individually compiled (though the SelectJavaCompile target isn't implemented yet in the MSBuild process). Remove the "Include In Project" and "Exclude From Project" commands since the source tree is based entirely on directory structure and folder specs.
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@471 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Language.Java/Project/JavaFileNode.cs
+++ Tvl.VisualStudio.Language.Java/Project/JavaFileNode.cs
@@ -1,18 +1,48 @@
 ﻿namespace Tvl.VisualStudio.Language.Java.Project
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using System.Runtime.InteropServices;
     using Microsoft.VisualStudio.Project;
-    using __VSHPROPID = Microsoft.VisualStudio.Shell.Interop.__VSHPROPID;
+    using Path = System.IO.Path;
+    using VsCommands2K = Microsoft.VisualStudio.VSConstants.VSStd2KCmdID;
+    using VSConstants = Microsoft.VisualStudio.VSConstants;
 
+    [ComVisible(true)]
     public class JavaFileNode : FileNode
     {
         public JavaFileNode(ProjectNode root, ProjectElement element)
             : base(root, element)
         {
-            SetProperty((int)__VSHPROPID.VSHPROPID_IsNonMemberItem, false);
+            IsNonMemberItem = false;
+        }
+
+        protected override int QueryStatusOnNode(Guid cmdGroup, uint cmd, IntPtr pCmdText, ref QueryStatusResult result)
+        {
+            if (cmdGroup == VsMenus.guidStandardCommandSet2K)
+            {
+                switch ((VsCommands2K)cmd)
+                {
+                case VsCommands2K.COMPILE:
+                    if (Path.GetExtension(FileName).Equals(".java", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // javac supports compiling a single source file
+                        result |= QueryStatusResult.SUPPORTED /*| QueryStatusResult.ENABLED*/;
+                        return VSConstants.S_OK;
+                    }
+
+                    break;
+
+                case VsCommands2K.INCLUDEINPROJECT:
+                case VsCommands2K.EXCLUDEFROMPROJECT:
+                    result |= QueryStatusResult.NOTSUPPORTED;
+                    return VSConstants.S_OK;
+
+                default:
+                    break;
+                }
+            }
+
+            return base.QueryStatusOnNode(cmdGroup, cmd, pCmdText, ref result);
         }
     }
 }
