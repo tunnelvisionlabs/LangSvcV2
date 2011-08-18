d0a39ba98f83cfd989b1a90580a2bdb90ad9da5e
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-08-18 14:35:18 +0000

    Mark JavaFolderNode as [ComVisible(true)]
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@472 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Language.Java/Project/JavaFolderNode.cs
+++ Tvl.VisualStudio.Language.Java/Project/JavaFolderNode.cs
@@ -1,12 +1,11 @@
 ﻿namespace Tvl.VisualStudio.Language.Java.Project
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using System.Runtime.InteropServices;
     using Microsoft.VisualStudio.Project;
+
     using __VSHPROPID = Microsoft.VisualStudio.Shell.Interop.__VSHPROPID;
 
+    [ComVisible(true)]
     public class JavaFolderNode : FolderNode
     {
         public JavaFolderNode(ProjectNode root, string relativePath, ProjectElement element)
