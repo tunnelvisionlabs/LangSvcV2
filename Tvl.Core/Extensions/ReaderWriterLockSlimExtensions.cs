f85c19eb6353f2befc9f7f4331c5334067b2dbdd
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-08-18 14:23:40 +0000

    Add extensions for ReaderWriterLockSlim operations with a timeout
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@463 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.Core/Extensions/ReaderWriterLockSlimExtensions.cs
+++ Tvl.Core/Extensions/ReaderWriterLockSlimExtensions.cs
@@ -13,6 +13,20 @@
             return new ReadLockHelper(readerWriterLock);
         }
 
+        public static ReadLockHelper ReadLock(this ReaderWriterLockSlim readerWriterLock, int millisecondsTimeout)
+        {
+            Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+            return new ReadLockHelper(readerWriterLock, millisecondsTimeout);
+        }
+
+        public static ReadLockHelper ReadLock(this ReaderWriterLockSlim readerWriterLock, TimeSpan timeout)
+        {
+            Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+            return new ReadLockHelper(readerWriterLock, timeout);
+        }
+
         public static UpgradeableReadLockHelper UpgradableReadLock(this ReaderWriterLockSlim readerWriterLock)
         {
             Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
@@ -20,11 +34,39 @@
             return new UpgradeableReadLockHelper(readerWriterLock);
         }
 
+        public static UpgradeableReadLockHelper UpgradableReadLock(this ReaderWriterLockSlim readerWriterLock, int millisecondsTimeout)
+        {
+            Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+            return new UpgradeableReadLockHelper(readerWriterLock, millisecondsTimeout);
+        }
+
+        public static UpgradeableReadLockHelper UpgradableReadLock(this ReaderWriterLockSlim readerWriterLock, TimeSpan timeout)
+        {
+            Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+            return new UpgradeableReadLockHelper(readerWriterLock, timeout);
+        }
+
         public static WriteLockHelper WriteLock(this ReaderWriterLockSlim readerWriterLock)
         {
             Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
 
             return new WriteLockHelper(readerWriterLock);
+        }
+
+        public static WriteLockHelper WriteLock(this ReaderWriterLockSlim readerWriterLock, int millisecondsTimeout)
+        {
+            Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+            return new WriteLockHelper(readerWriterLock, millisecondsTimeout);
+        }
+
+        public static WriteLockHelper WriteLock(this ReaderWriterLockSlim readerWriterLock, TimeSpan timeout)
+        {
+            Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+            return new WriteLockHelper(readerWriterLock, timeout);
         }
 
         public struct ReadLockHelper : IDisposable
@@ -36,6 +78,26 @@
                 Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
 
                 readerWriterLock.EnterReadLock();
+                this._readerWriterLock = readerWriterLock;
+            }
+
+            public ReadLockHelper(ReaderWriterLockSlim readerWriterLock, int millisecondsTimeout)
+            {
+                Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+                if (!readerWriterLock.TryEnterReadLock(millisecondsTimeout))
+                    throw new TimeoutException();
+
+                this._readerWriterLock = readerWriterLock;
+            }
+
+            public ReadLockHelper(ReaderWriterLockSlim readerWriterLock, TimeSpan timeout)
+            {
+                Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+                if (!readerWriterLock.TryEnterReadLock(timeout))
+                    throw new TimeoutException();
+
                 this._readerWriterLock = readerWriterLock;
             }
 
@@ -57,6 +119,26 @@
                 this._readerWriterLock = readerWriterLock;
                 this._readonly = false;
                 this._readerWriterLock.EnterUpgradeableReadLock();
+            }
+
+            public UpgradeableReadLockHelper(ReaderWriterLockSlim readerWriterLock, int millisecondsTimeout)
+            {
+                Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+                this._readerWriterLock = readerWriterLock;
+                this._readonly = false;
+                if (!this._readerWriterLock.TryEnterUpgradeableReadLock(millisecondsTimeout))
+                    throw new TimeoutException();
+            }
+
+            public UpgradeableReadLockHelper(ReaderWriterLockSlim readerWriterLock, TimeSpan timeout)
+            {
+                Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+                this._readerWriterLock = readerWriterLock;
+                this._readonly = false;
+                if (!this._readerWriterLock.TryEnterUpgradeableReadLock(timeout))
+                    throw new TimeoutException();
             }
 
             public bool IsReadOnly
@@ -97,6 +179,26 @@
                 Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
 
                 readerWriterLock.EnterWriteLock();
+                this._readerWriterLock = readerWriterLock;
+            }
+
+            public WriteLockHelper(ReaderWriterLockSlim readerWriterLock, int millisecondsTimeout)
+            {
+                Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+                if (!readerWriterLock.TryEnterWriteLock(millisecondsTimeout))
+                    throw new TimeoutException();
+
+                this._readerWriterLock = readerWriterLock;
+            }
+
+            public WriteLockHelper(ReaderWriterLockSlim readerWriterLock, TimeSpan timeout)
+            {
+                Contract.Requires<ArgumentNullException>(readerWriterLock != null, "readerWriterLock");
+
+                if (!readerWriterLock.TryEnterWriteLock(timeout))
+                    throw new TimeoutException();
+
                 this._readerWriterLock = readerWriterLock;
             }
 
