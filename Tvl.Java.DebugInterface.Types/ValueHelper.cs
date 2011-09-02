074c749070626044a9365307f6b3109d31961741
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-09-02 22:43:54 +0000

    Implement float/int conversion (and back)
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@522 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.Java.DebugInterface.Types/ValueHelper.cs
+++ Tvl.Java.DebugInterface.Types/ValueHelper.cs
@@ -1,17 +1,40 @@
 ﻿namespace Tvl.Java.DebugInterface.Types
 {
-    using System;
+    using System.Runtime.InteropServices;
 
     public static class ValueHelper
     {
         public static int SingleToInt32Bits(float value)
         {
-            throw new NotImplementedException();
+            FloatConversionStruct fcs = new FloatConversionStruct(value);
+            return fcs.IntValue;
         }
 
         public static float Int32BitsToSingle(int value)
         {
-            throw new NotImplementedException();
+            FloatConversionStruct fcs = new FloatConversionStruct(value);
+            return fcs.FloatValue;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct FloatConversionStruct
+        {
+            [FieldOffset(0)]
+            public int IntValue;
+            [FieldOffset(0)]
+            public float FloatValue;
+
+            public FloatConversionStruct(int value)
+                : this()
+            {
+                IntValue = value;
+            }
+
+            public FloatConversionStruct(float value)
+                : this()
+            {
+                FloatValue = value;
+            }
         }
     }
 }
