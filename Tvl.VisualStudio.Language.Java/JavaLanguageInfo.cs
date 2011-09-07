be4ee3bc11d785b021a1c81e766bf37d8ce23a4b
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-09-07 03:26:56 +0000

    The position returned by ValidateBreakpointLocation now starts at the first non-whitespace character
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@548 58fa913a-3432-0140-a220-d8bfcf8ade31

--- Tvl.VisualStudio.Language.Java/JavaLanguageInfo.cs
+++ Tvl.VisualStudio.Language.Java/JavaLanguageInfo.cs
@@ -1,12 +1,16 @@
 ﻿namespace Tvl.VisualStudio.Language.Java
 {
-    using System;
     using System.Collections.Generic;
     using System.Runtime.InteropServices;
-    using Microsoft.VisualStudio;
-    using Microsoft.VisualStudio.Shell;
-    using Microsoft.VisualStudio.TextManager.Interop;
+    using Microsoft.VisualStudio.Text;
+    using Tvl.VisualStudio.Shell.Extensions;
     using Tvl.VisualStudio.Text;
+
+    using IVsEditorAdaptersFactoryService = Microsoft.VisualStudio.Editor.IVsEditorAdaptersFactoryService;
+    using IVsTextBuffer = Microsoft.VisualStudio.TextManager.Interop.IVsTextBuffer;
+    using SVsServiceProvider = Microsoft.VisualStudio.Shell.SVsServiceProvider;
+    using TextSpan = Microsoft.VisualStudio.TextManager.Interop.TextSpan;
+    using VSConstants = Microsoft.VisualStudio.VSConstants;
 
     [Guid(Constants.JavaLanguageGuidString)]
     internal class JavaLanguageInfo : LanguageInfo
@@ -34,12 +38,19 @@
 
         public override int ValidateBreakpointLocation(IVsTextBuffer buffer, int line, int col, TextSpan[] pCodeSpan)
         {
+            var componentModel = ServiceProvider.GetComponentModel();
+            var adapterFactoryService = componentModel.DefaultExportProvider.GetExport<IVsEditorAdaptersFactoryService>();
+            ITextBuffer textBuffer = adapterFactoryService.Value.GetDataBuffer(buffer);
+
+            ITextSnapshot snapshot = textBuffer.CurrentSnapshot;
+            ITextSnapshotLine snapshotLine = snapshot.GetLineFromLineNumber(line);
+            string lineText = snapshotLine.GetText();
+
             // allow any single line to have a breakpoint (no parser-based location validation)
             pCodeSpan[0].iStartLine = line;
-            pCodeSpan[0].iStartIndex = 0;
+            pCodeSpan[0].iStartIndex = snapshotLine.Length - lineText.TrimStart().Length;
             pCodeSpan[0].iEndLine = line;
-            ErrorHandler.ThrowOnFailure(buffer.GetLengthOfLine(line, out pCodeSpan[0].iEndIndex));
-
+            pCodeSpan[0].iEndIndex = snapshotLine.Length;
             return VSConstants.S_OK;
         }
     }
