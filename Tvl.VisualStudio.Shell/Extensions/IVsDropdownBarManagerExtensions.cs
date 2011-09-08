9fd584b8ce8f9a54a95639e20bfa3e88a20e24bd
sam <sam@58fa913a-3432-0140-a220-d8bfcf8ade31>
2011-09-08 22:04:36 +0000

    New extensions (IVsCodeWindow, IVsDropdownBarManager, IVsTextBuffer)
    git-svn-id: https://terabyte:8443/svn/LanguageServices/trunk@566 58fa913a-3432-0140-a220-d8bfcf8ade31

---
+++ Tvl.VisualStudio.Shell/Extensions/IVsDropdownBarManagerExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Tvl.VisualStudio.Shell.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using IVsDropdownBarClient = Microsoft.VisualStudio.TextManager.Interop.IVsDropdownBarClient;
+    using IVsDropdownBarManager = Microsoft.VisualStudio.TextManager.Interop.IVsDropdownBarManager;
+    using IVsDropdownBar = Microsoft.VisualStudio.TextManager.Interop.IVsDropdownBar;
+    using System.Diagnostics.Contracts;
+    using Microsoft.VisualStudio;
+
+    public static class IVsDropdownBarManagerExtensions
+    {
+        public static IVsDropdownBar GetDropdownBar(this IVsDropdownBarManager dropdownBarManager)
+        {
+            Contract.Requires<ArgumentNullException>(dropdownBarManager != null, "dropdownBarManager");
+
+            IVsDropdownBar dropdownBar;
+            if (ErrorHandler.Failed(dropdownBarManager.GetDropdownBar(out dropdownBar)))
+                return null;
+
+            return dropdownBar;
+        }
+
+        public static IVsDropdownBarClient GetDropdownBarClient(this IVsDropdownBarManager dropdownBarManager)
+        {
+            Contract.Requires<ArgumentNullException>(dropdownBarManager != null, "dropdownBarManager");
+
+            IVsDropdownBar dropdownBar = GetDropdownBar(dropdownBarManager);
+            IVsDropdownBarClient client;
+            if (dropdownBar == null || ErrorHandler.Failed(dropdownBar.GetClient(out client)))
+                return null;
+
+            return client;
+        }
+    }
+}
---
+++ Tvl.VisualStudio.Shell/Extensions/IVsTextBufferExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Tvl.VisualStudio.Shell.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using IVsTextBuffer = Microsoft.VisualStudio.TextManager.Interop.IVsTextBuffer;
+    using System.Diagnostics.Contracts;
+    using Microsoft.VisualStudio;
+
+    public static class IVsTextBufferExtensions
+    {
+        public static Guid? GetLanguageServiceID(this IVsTextBuffer textBuffer)
+        {
+            Contract.Requires<ArgumentNullException>(textBuffer != null, "textBuffer");
+
+            Guid id;
+            int hr = textBuffer.GetLanguageServiceID(out id);
+            if (hr != VSConstants.S_OK)
+                return null;
+
+            return id;
+        }
+    }
+}
