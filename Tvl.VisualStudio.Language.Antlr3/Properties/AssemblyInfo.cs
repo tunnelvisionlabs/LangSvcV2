4d39eaeae3b392ec974621101d091265a1ecad2c
Sam Harwell <sam@tunnelvisionlabs.com>
2012-07-05 16:53:08 -0500

    Update assembly and extension version numbers for a new release of the ANTLR 3 and StringTemplate language extensions

--- Tvl.VisualStudio.Language.Antlr3/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.Language.Antlr3/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
 [assembly: AssemblyProduct("Tvl.VisualStudio.Language.Antlr3")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2010")]
+[assembly: AssemblyCopyright("Copyright © Sam Harwell 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyFileVersion("1.0.11.0")]
