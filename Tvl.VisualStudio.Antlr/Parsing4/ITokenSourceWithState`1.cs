8c5ccebeb69228b9f19bccadc01ccef198a250a9
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-26 22:13:46 -0600

    Copy the ANTLR 3 parsing utility classes to the Parsing4 namespace for working with ANTLR 4

---
+++ Tvl.VisualStudio.Antlr/Parsing4/ITokenSourceWithState`1.cs
@@ -0,0 +1,15 @@
+﻿namespace Tvl.VisualStudio.Language.Parsing4
+{
+    using Antlr.Runtime;
+
+    public interface ITokenSourceWithState<T> : ITokenSource
+        where T : struct
+    {
+        ICharStream CharStream
+        {
+            get;
+        }
+
+        T GetCurrentState();
+    }
+}
