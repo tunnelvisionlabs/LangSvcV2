78538a38f8ed2a6784ad11f305ae9230b0241d6e
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-26 22:28:04 -0600

    Update Inheritance Margin assembly info

--- Tvl.VisualStudio.InheritanceMargin/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.InheritanceMargin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
 [assembly: AssemblyProduct("Tvl.VisualStudio.InheritanceMargin")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2012")]
+[assembly: AssemblyCopyright("Copyright © Sam Harwell 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
