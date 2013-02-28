57cb4de17d2b78e2c6763515a5b7cd1b6ed8d629
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-28 11:08:27 -0600

    Add an implementation of SimpleLexerState for ANTLR 4

---
+++ Tvl.VisualStudio.Antlr/Parsing4/SimpleLexerState.cs
@@ -0,0 +1,88 @@
+﻿namespace Tvl.VisualStudio.Language.Parsing4
+{
+    using System;
+    using Antlr4.Runtime;
+    using Tvl.Collections;
+
+    public struct SimpleLexerState : IEquatable<SimpleLexerState>
+    {
+        private static readonly int[] EmptyModeStack = { };
+        private static readonly SimpleLexerState _initial = new SimpleLexerState(Lexer.DefaultMode);
+
+        private readonly int _mode;
+        private readonly int[] _modeStack;
+
+        public SimpleLexerState(Lexer lexer)
+        {
+            _mode = lexer._mode;
+            if (lexer._modeStack.Count == 0)
+                _modeStack = EmptyModeStack;
+            else
+                _modeStack = lexer._modeStack.ToArray();
+        }
+
+        private SimpleLexerState(int mode)
+        {
+            _mode = mode;
+            _modeStack = EmptyModeStack;
+        }
+
+        private SimpleLexerState(int mode, int[] modeStack)
+        {
+            _mode = mode;
+            _modeStack = modeStack;
+        }
+
+        public static SimpleLexerState Initial
+        {
+            get
+            {
+                return _initial;
+            }
+        }
+
+        public int Mode
+        {
+            get
+            {
+                return _mode;
+            }
+        }
+
+        public int[] ModeStack
+        {
+            get
+            {
+                return _modeStack;
+            }
+        }
+
+        public void Apply(Lexer lexer)
+        {
+            lexer._mode = this.Mode;
+            lexer._modeStack.AddRange(_modeStack);
+        }
+
+        public bool Equals(SimpleLexerState other)
+        {
+            return _mode == other._mode
+                && ArrayEqualityComparers.Int32.Equals(_modeStack, other._modeStack);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is SimpleLexerState))
+                return false;
+
+            return Equals((SimpleLexerState)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1;
+            hashCode = hashCode * 31 + _mode;
+            hashCode = hashCode * 31 + ArrayEqualityComparers.Int32.GetHashCode(_modeStack);
+            return hashCode;
+        }
+    }
+}
