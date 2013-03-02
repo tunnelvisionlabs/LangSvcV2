f886c2366303b528fcc20cf8fbda5f0d7f3d3a1d
Sam Harwell <sam@tunnelvisionlabs.com>
2013-03-02 15:16:35 -0600

    Enable the ANTLR 4-based classifier for .php5 files for easy side-by-side comparison with the previous ANTLR 3-based classifier

--- Tvl.VisualStudio.Language.Php/Classification/PhpClassifierProvider.cs
+++ Tvl.VisualStudio.Language.Php/Classification/PhpClassifierProvider.cs
@@ -5,13 +5,32 @@
     using Microsoft.VisualStudio.Text.Classification;
     using Microsoft.VisualStudio.Utilities;
     using Tvl.VisualStudio.Text.Classification;
+    using StringComparison = System.StringComparison;
+    using Path = System.IO.Path;
 
     [Export(typeof(IClassifierProvider))]
     [ContentType(PhpConstants.PhpContentType)]
     public sealed class PhpClassifierProvider : LanguageClassifierProvider<PhpLanguagePackage>
     {
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService
+        {
+            get;
+            private set;
+        }
+
         protected override IClassifier GetClassifierImpl(ITextBuffer textBuffer)
         {
+            ITextDocument textDocument;
+            if (TextDocumentFactoryService.TryGetTextDocument(textBuffer, out textDocument))
+            {
+                if (!string.IsNullOrEmpty(textDocument.FilePath)
+                    && Path.GetExtension(textDocument.FilePath).Equals(PhpConstants.Php5FileExtension, StringComparison.OrdinalIgnoreCase))
+                {
+                    return new V4PhpClassifier(textBuffer, StandardClassificationService, ClassificationTypeRegistryService);
+                }
+            }
+
             return new PhpClassifier(textBuffer, StandardClassificationService, ClassificationTypeRegistryService);
         }
     }
