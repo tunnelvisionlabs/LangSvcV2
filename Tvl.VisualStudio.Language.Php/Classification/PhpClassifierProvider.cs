b594c8246bfa2e0a50a566f2210b4a7ea7e4c978
Sam Harwell <sam@tunnelvisionlabs.com>
2013-03-03 21:50:05 -0600

    Make sure to pass the root document to TryGetTextDocument

--- Tvl.VisualStudio.Language.Php/Classification/PhpClassifierProvider.cs
+++ Tvl.VisualStudio.Language.Php/Classification/PhpClassifierProvider.cs
@@ -1,12 +1,14 @@
 ﻿namespace Tvl.VisualStudio.Language.Php.Classification
 {
     using System.ComponentModel.Composition;
+    using System.Linq;
     using Microsoft.VisualStudio.Text;
     using Microsoft.VisualStudio.Text.Classification;
+    using Microsoft.VisualStudio.Text.Projection;
     using Microsoft.VisualStudio.Utilities;
     using Tvl.VisualStudio.Text.Classification;
-    using StringComparison = System.StringComparison;
     using Path = System.IO.Path;
+    using StringComparison = System.StringComparison;
 
     [Export(typeof(IClassifierProvider))]
     [ContentType(PhpConstants.PhpContentType)]
@@ -19,10 +21,23 @@
             private set;
         }
 
+        [Import]
+        public IBufferGraphFactoryService BufferGraphFactoryService
+        {
+            get;
+            private set;
+        }
+
         protected override IClassifier GetClassifierImpl(ITextBuffer textBuffer)
         {
+            if (!textBuffer.ContentType.IsOfType(PhpConstants.PhpContentType))
+                return null;
+
+            IBufferGraph graph = BufferGraphFactoryService.CreateBufferGraph(textBuffer);
+            var rootBuffers = graph.GetTextBuffers(buffer => !(buffer is IProjectionBufferBase));
+
             ITextDocument textDocument;
-            if (TextDocumentFactoryService.TryGetTextDocument(textBuffer, out textDocument))
+            if (TextDocumentFactoryService.TryGetTextDocument(rootBuffers.FirstOrDefault() ?? textBuffer, out textDocument))
             {
                 if (!string.IsNullOrEmpty(textDocument.FilePath)
                     && Path.GetExtension(textDocument.FilePath).Equals(PhpConstants.Php5FileExtension, StringComparison.OrdinalIgnoreCase))
