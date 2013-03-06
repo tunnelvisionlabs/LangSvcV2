c5ae724fe260640d11259bff561ec6ee2f77c522
Sam Harwell <sam@tunnelvisionlabs.com>
2013-03-05 21:44:25 -0600

    Added Apache 2 license notices to code derived from the Python Tools project.

--- Tvl.VisualStudio.Language.Php/TemplateTokenKind.cs
+++ Tvl.VisualStudio.Language.Php/TemplateTokenKind.cs
@@ -1,4 +1,20 @@
-﻿namespace Tvl.VisualStudio.Language.Php
+﻿/* ****************************************************************************
+ *
+ * Copyright (c) Microsoft Corporation. 
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ * copy of the license can be found in the License.html file at the root of this distribution. If 
+ * you cannot locate the Apache License, Version 2.0, please send an email to 
+ * vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * by the terms of the Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * This source code has been modified from its original form.
+ *
+ * ***************************************************************************/
+
+namespace Tvl.VisualStudio.Language.Php
 {
     internal enum TemplateTokenKind
     {
