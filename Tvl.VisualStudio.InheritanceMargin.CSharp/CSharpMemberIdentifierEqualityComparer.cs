1bef5fdff641fc8f53674aa20abceae56e306be0
Sam Harwell <sam@tunnelvisionlabs.com>
2013-08-02 20:29:23 -0500

    Extract nested classes in CSharpInheritanceAnalyzer to their own top-level types

---
+++ Tvl.VisualStudio.InheritanceMargin.CSharp/CSharpMemberIdentifierEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿namespace Tvl.VisualStudio.InheritanceMargin.CSharp
+{
+    using System.Collections.Generic;
+    using Microsoft.RestrictedUsage.CSharp.Semantics;
+
+    internal class CSharpMemberIdentifierEqualityComparer : IEqualityComparer<CSharpMemberIdentifier>
+    {
+        public bool Equals(CSharpMemberIdentifier left, CSharpMemberIdentifier right)
+        {
+            if (object.ReferenceEquals(left, right))
+                return true;
+
+            if (object.ReferenceEquals(left, null) || object.ReferenceEquals(right, null))
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public int GetHashCode(CSharpMemberIdentifier identifier)
+        {
+            return identifier.GetHashCode();
+        }
+    }
+}
---
+++ Tvl.VisualStudio.InheritanceMargin.CSharp/TypeTarget.cs
@@ -0,0 +1,29 @@
+﻿namespace Tvl.VisualStudio.InheritanceMargin.CSharp
+{
+    using Microsoft.RestrictedUsage.CSharp.Semantics;
+
+    internal sealed class TypeTarget : IInheritanceTarget
+    {
+        private readonly string _displayName;
+        private readonly CSharpTypeIdentifier _typeIdentifier;
+
+        public TypeTarget(string displayName, CSharpTypeIdentifier typeIdentifier)
+        {
+            _displayName = displayName;
+            _typeIdentifier = typeIdentifier;
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+        }
+
+        public void NavigateTo()
+        {
+            CSharpInheritanceAnalyzer.NavigateToType(_typeIdentifier);
+        }
+    }
+}
