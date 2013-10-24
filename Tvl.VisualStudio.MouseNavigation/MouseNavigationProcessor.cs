3d2d5acd038bc16e918566933b01b5e7597b8e9c
Sam Harwell <sam@tunnelvisionlabs.com>
2013-10-24 16:31:04 -0500

    Fix unreliable behavior of back/forward buttons in Mouse Navigation extension

--- Tvl.VisualStudio.MouseNavigation/MouseNavigationProcessor.cs
+++ Tvl.VisualStudio.MouseNavigation/MouseNavigationProcessor.cs
@@ -8,11 +8,9 @@
     using IWpfTextView = Microsoft.VisualStudio.Text.Editor.IWpfTextView;
     using MouseButton = System.Windows.Input.MouseButton;
     using MouseButtonEventArgs = System.Windows.Input.MouseButtonEventArgs;
-    using MouseButtonState = System.Windows.Input.MouseButtonState;
     using MouseProcessorBase = Microsoft.VisualStudio.Text.Editor.MouseProcessorBase;
     using OLECMDEXECOPT = Microsoft.VisualStudio.OLE.Interop.OLECMDEXECOPT;
     using SVsUIShell = Microsoft.VisualStudio.Shell.Interop.SVsUIShell;
-    using UIElement = System.Windows.UIElement;
     using VSConstants = Microsoft.VisualStudio.VSConstants;
 
     internal class MouseNavigationProcessor : MouseProcessorBase
@@ -21,9 +19,6 @@
         {
             TextView = wpfTextView;
             ServiceProvider = serviceProvider;
-            UIElement element = TextView as UIElement;
-            if (element != null)
-                element.MouseDown += UIElement_MouseDown;
         }
 
         private IWpfTextView TextView
@@ -38,19 +33,21 @@
             set;
         }
 
-        private void UIElement_MouseDown(object sender, MouseButtonEventArgs e)
+        public override void PostprocessMouseDown(MouseButtonEventArgs e)
         {
-            if (e.ButtonState != MouseButtonState.Pressed)
-                return;
-
-            uint cmdId = (uint)VSConstants.VSStd97CmdID.ShellNavBackward;
-            if (e.ChangedButton == MouseButton.XButton1)
+            uint cmdId;
+            switch (e.ChangedButton)
             {
+            case MouseButton.XButton1:
                 cmdId = (uint)VSConstants.VSStd97CmdID.ShellNavBackward;
-            }
-            else if (e.ChangedButton == MouseButton.XButton2)
-            {
+                break;
+
+            case MouseButton.XButton2:
                 cmdId = (uint)VSConstants.VSStd97CmdID.ShellNavForward;
+                break;
+
+            default:
+                return;
             }
 
             try
@@ -60,6 +57,7 @@
                 OLECMDEXECOPT cmdExecOpt = OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER;
                 object obj = null;
                 ErrorHandler.ThrowOnFailure(shell.PostExecCommand(cmdGroup, cmdId, (uint)cmdExecOpt, ref obj));
+                e.Handled = true;
             }
             catch (COMException)
             {
