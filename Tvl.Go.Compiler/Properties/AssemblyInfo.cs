6c1e8d56aa646d34e544b446316ad0eca44b0240
Sam Harwell <sam@tunnelvisionlabs.com>
2014-02-18 08:31:15 -0600

    Mark Tvl.Go.Compiler assembly as not CLS compliant

--- Tvl.Go.Compiler/Properties/AssemblyInfo.cs
+++ Tvl.Go.Compiler/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -17,6 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8f0beec5-6b5f-4ea3-8989-851da6e00825")]
