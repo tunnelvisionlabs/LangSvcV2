a864a66bb43051192a7eef695958ff375ef99ed1
Sam Harwell <sam@tunnelvisionlabs.com>
2014-09-13 13:42:11 -0500

    Remove the MPFProj source code since it's no longer used

--- External/ProjectBase2010/ExternDll.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.VisualStudio.Project
-{
-    internal static class ExternDll
-    {
-        public const string Kernel32 = "kernel32.dll";
-        public const string Shell32 = "shell32.dll";
-        public const string User32 = "user32.dll";
-    }
-}
