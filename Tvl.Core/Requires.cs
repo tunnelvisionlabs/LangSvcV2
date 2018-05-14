84edb1725355c87b7327eae2a44501d716a6e4d5
Sam Harwell <sam@tunnelvisionlabs.com>
2018-05-14 10:24:20 -0500

    Add Tvl.Verify helper class

--- Tvl.Core/Requires.cs
+++ Tvl.Core/Requires.cs
@@ -9,8 +9,6 @@ namespace Tvl
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Globalization;
-    using System.Reflection;
     using System.Runtime;
     using System.Threading.Tasks;
 
@@ -409,41 +407,6 @@ namespace Tvl
         private static string Format(string format, params object[] arguments)
         {
             return PrivateErrorHelpers.Format(format, arguments);
-        }
-
-        /// <summary>
-        /// Common utility methods used by the various error detection and reporting classes.
-        /// </summary>
-        private static class PrivateErrorHelpers
-        {
-            /// <summary>
-            /// Trims away a given surrounding type, returning just the generic type argument,
-            /// if the given type is in fact a generic type with just one type argument and
-            /// the generic type matches a given wrapper type.  Otherwise, it returns the original type.
-            /// </summary>
-            /// <param name="type">The type to trim, or return unmodified.</param>
-            /// <param name="wrapper">The SomeType&lt;&gt; generic type definition to trim away from <paramref name="type"/> if it is present.</param>
-            /// <returns><paramref name="type"/>, if it is not a generic type instance of <paramref name="wrapper"/>; otherwise the type argument.</returns>
-            internal static Type TrimGenericWrapper(Type type, Type wrapper)
-            {
-                Type[] typeArgs;
-                if (type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == wrapper && (typeArgs = type.GenericTypeArguments).Length == 1)
-                {
-                    return typeArgs[0];
-                }
-                else
-                {
-                    return type;
-                }
-            }
-
-            /// <summary>
-            /// Helper method that formats string arguments.
-            /// </summary>
-            internal static string Format(string format, params object[] arguments)
-            {
-                return string.Format(CultureInfo.CurrentCulture, format, arguments);
-            }
         }
 
         private static class Strings
