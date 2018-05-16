0a05da9aab9bf7105bc5d188dd7afc506af0c0da
Sam Harwell <sam@tunnelvisionlabs.com>
2018-05-16 08:34:35 -0500

    Add ProvideCodeBase for output window interfaces assembly

--- Tvl.VisualStudio.Language.Alloy/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.Language.Alloy/Properties/AssemblyInfo.cs
@@ -42,6 +42,7 @@ using Microsoft.VisualStudio.Shell;
 [assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.Antlr")]
 [assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.Language")]
 [assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.Language.Interfaces")]
+[assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.OutputWindow.Interfaces")]
 [assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.Shell")]
 [assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.Text")]
 [assembly: ProvideCodeBase(AssemblyName = "Tvl.VisualStudio.Text.Interfaces")]
