16ca94b67b814059298f71dbd04d658ab5935292
Sam Harwell <sam@tunnelvisionlabs.com>
2020-02-08 21:51:01 -0800

    Convert template projects to SDK-style projects

--- Templates/Antlr/ItemTemplates/LexerGrammarItemTemplate/Properties/AssemblyInfo.cs
+++ Templates/Antlr/ItemTemplates/LexerGrammarItemTemplate/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("LexerGrammarItemTemplate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
-[assembly: AssemblyProduct("LexerGrammarItemTemplate")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8d6a6d26-a106-4e12-aec4-fbc58c5930c3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.594")]
-[assembly: AssemblyFileVersion("1.0.1.594")]
--- Templates/Antlr/ItemTemplates/TreeGrammarItemTemplate/Properties/AssemblyInfo.cs
+++ Templates/Antlr/ItemTemplates/TreeGrammarItemTemplate/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TreeGrammarItemTemplate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
-[assembly: AssemblyProduct("TreeGrammarItemTemplate")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("34809864-7fdf-4f08-96f1-6810cafdcf57")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.594")]
-[assembly: AssemblyFileVersion("1.0.1.594")]
